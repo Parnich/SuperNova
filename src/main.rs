fc6e107e0f9a757725d38509aa51442d3cbfe94b
Parnich <parn1ch@yandex.ru>
2026-06-07 20:50:47 +0400

    Connection implementation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use futures::StreamExt;
+use rand::RngCore;
 use ratatui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
@@ -12,6 +13,7 @@ use ratatui::{
     Terminal,
 };
 use std::{io, net::SocketAddr};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use zeroize::{Zeroize, ZeroizeOnDrop};
 
@@ -25,12 +27,15 @@ enum AppState {
     Connected {
         peer_addr: SocketAddr,
         peer_name: String,
+        tx: tokio::sync::mpsc::Sender<Vec<u8>>,
     },
 }
 
 enum AppEvent {
     Terminal(KeyEvent),
     NetworkNewConnection(tokio::net::TcpStream, SocketAddr),
+    NetworkMessage(String),
+    NetworkDisconnected,
     Error(String),
 }
 
@@ -101,7 +106,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let status_string = match &app_state {
                 AppState::Listening => "Listening for incoming connections on port 9099...".to_string(),
-                AppState::Connected { peer_name, peer_addr } => format!("Connected with @{} ({})", peer_name, peer_addr),
+                AppState::Connected { peer_name, peer_addr, .. } => format!("Connected with @{} ({})", peer_name, peer_addr),
             };
             let status = Paragraph::new(status_string)
                 .style(Style::default().fg(Color::Green));
@@ -138,8 +143,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             }
                             KeyCode::Enter => {
                                 if !input_text.is_empty() {
-                                    chat_history.push(format!("[You]: {}", input_text));
-                                    input_text.clear();
+                                    if input_text.starts_with("/connect ") {
+                                        let addr_str = input_text.trim_start_matches("/connect ").to_string();
+                                        chat_history.push(format!("[System]: Connecting to {}...", addr_str));
+                                        input_text.clear();
+                                        let connect_tx = event_tx.clone();
+                                        tokio::spawn(async move {
+                                            if let Ok(addr) = addr_str.parse::<SocketAddr>() {
+                                                match tokio::net::TcpStream::connect(addr).await {
+                                                    Ok(stream) => {
+                                                        let _ = connect_tx.send(AppEvent::NetworkNewConnection(stream, addr)).await;
+                                                    }
+                                                    Err(e) => {
+                                                        let _ = connect_tx.send(AppEvent::Error(e.to_string())).await;
+                                                    }
+                                                }
+                                            } else {
+                                                let _ = connect_tx.send(AppEvent::Error("Invalid address format".to_string())).await;
+                                            }
+                                        });
+                                    } else {
+                                        match &app_state {
+                                            AppState::Connected { tx, .. } => {
+                                                let _ = tx.send(input_text.as_bytes().to_vec()).await;
+                                                chat_history.push(format!("[You]: {}", input_text));
+                                                input_text.clear();
+                                            }
+                                            AppState::Listening => {
+                                                chat_history.push("[System]: Not connected. Use /connect <ip>:<port>".to_string());
+                                                input_text.clear();
+                                            }
+                                        }
+                                    }
                                 }
                             }
                             _ => {}
@@ -149,20 +184,73 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 AppEvent::NetworkNewConnection(stream, addr) => {
                     match app_state {
                         AppState::Listening => {
+                            let (net_send_tx, mut net_send_rx) = tokio::sync::mpsc::channel::<Vec<u8>>(100);
+                            let main_tx = event_tx.clone();
+
                             app_state = AppState::Connected {
                                 peer_addr: addr,
-                                peer_name: "Unknown_Peer".to_string(),
+                                peer_name: "Peer".to_string(),
+                                tx: net_send_tx,
                             };
-                            chat_history.push(format!("[System]: Peer [{}] detected. Establishing encrypted session...", addr));
+                            chat_history.push(format!("[System]: Successfully connected with [{}].", addr));
 
+                            let (mut reader, mut writer) = stream.into_split();
                             tokio::spawn(async move {
-                                let _sh = stream;
+                                let mut read_buf = [0u8; 512];
+                                loop {
+                                    tokio::select! {
+                                        res = reader.read_exact(&mut read_buf) => {
+                                            match res {
+                                                Ok(_) => {
+                                                    let packet_type = read_buf[0];
+                                                    if packet_type == 1 {
+                                                        let len = u16::from_be_bytes([read_buf[1], read_buf[2]]) as usize;
+                                                        if len <= 509 {
+                                                            if let Ok(msg) = String::from_utf8(read_buf[3..3+len].to_vec()) {
+                                                                let _ = main_tx.send(AppEvent::NetworkMessage(msg)).await;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                                Err(_) => {
+                                                    let _ = main_tx.send(AppEvent::NetworkDisconnected).await;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        Some(msg_bytes) = net_send_rx.recv() => {
+                                            let mut write_buf = [0u8; 512];
+                                            write_buf[0] = 1;
+                                            let len = msg_bytes.len().min(509);
+                                            let len_bytes = (len as u16).to_be_bytes();
+                                            write_buf[1] = len_bytes[0];
+                                            write_buf[2] = len_bytes[1];
+                                            write_buf[3..3+len].copy_from_slice(&msg_bytes[..len]);
+                                            rand::thread_rng().fill_bytes(&mut write_buf[3+len..]);
+                                            if writer.write_all(&write_buf).await.is_err() {
+                                                let _ = main_tx.send(AppEvent::NetworkDisconnected).await;
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
                             });
                         }
                         AppState::Connected { .. } => {
                             drop(stream);
                         }
                     }
+                }
+                AppEvent::NetworkMessage(msg) => {
+                    let name = match &app_state {
+                        AppState::Connected { peer_name, .. } => peer_name.clone(),
+                        _ => "Peer".to_string(),
+                    };
+                    chat_history.push(format!("[{}]: {}", name, msg));
+                }
+                AppEvent::NetworkDisconnected => {
+                    chat_history.push("[System]: Connection closed by peer.".to_string());
+                    app_state = AppState::Listening;
                 }
                 AppEvent::Error(err) => {
                     chat_history.push(format!("[Error]: {}", err));
